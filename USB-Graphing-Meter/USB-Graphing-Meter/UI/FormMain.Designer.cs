8531e104564ed37978a85319d4c6f8c3a40c98d1
RichardWang919 <46060115+RichardWang919@users.noreply.github.com>
2022-08-08 09:24:12 +0800

    Add files via upload

---
+++ USB-Graphing-Meter/USB-Graphing-Meter/UI/FormMain.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace GraphingMeter
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelSampleRate = new System.Windows.Forms.ToolStripStatusLabel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lb_voltage = new System.Windows.Forms.Label();
+            this.tb_vcc = new System.Windows.Forms.TextBox();
+            this.statusStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelSampleRate});
+            this.statusStrip.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.Flow;
+            this.statusStrip.Location = new System.Drawing.Point(0, 342);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(584, 20);
+            this.statusStrip.TabIndex = 1;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelSampleRate
+            // 
+            this.toolStripStatusLabelSampleRate.Name = "toolStripStatusLabelSampleRate";
+            this.toolStripStatusLabelSampleRate.Size = new System.Drawing.Size(190, 15);
+            this.toolStripStatusLabelSampleRate.Text = "toolStripStatusLabelSampleRate";
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea2.AxisX.LabelStyle.Enabled = false;
+            chartArea2.AxisX.MajorGrid.LineColor = System.Drawing.Color.White;
+            chartArea2.AxisX.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chartArea2.AxisY.MajorGrid.LineColor = System.Drawing.Color.White;
+            chartArea2.AxisY.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chartArea2.AxisY.Maximum = 5D;
+            chartArea2.AxisY.Minimum = 0D;
+            chartArea2.BackColor = System.Drawing.Color.Black;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Fire;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.MarkerSize = 1024;
+            series2.Name = "Volts";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(584, 339);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "voltageChart";
+            // 
+            // lb_voltage
+            // 
+            this.lb_voltage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb_voltage.AutoSize = true;
+            this.lb_voltage.Location = new System.Drawing.Point(526, 326);
+            this.lb_voltage.Name = "lb_voltage";
+            this.lb_voltage.Size = new System.Drawing.Size(38, 13);
+            this.lb_voltage.TabIndex = 3;
+            this.lb_voltage.Text = "0.00 V";
+            this.lb_voltage.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tb_vcc
+            // 
+            this.tb_vcc.Location = new System.Drawing.Point(402, 323);
+            this.tb_vcc.Name = "tb_vcc";
+            this.tb_vcc.Size = new System.Drawing.Size(46, 20);
+            this.tb_vcc.TabIndex = 4;
+            this.tb_vcc.Text = "4.87";
+            this.tb_vcc.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 362);
+            this.Controls.Add(this.tb_vcc);
+            this.Controls.Add(this.lb_voltage);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.statusStrip);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Graphing Meter";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormTerminal_FormClosed);
+            this.Load += new System.EventHandler(this.FormTerminal_Load);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelSampleRate;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label lb_voltage;
+        private System.Windows.Forms.TextBox tb_vcc;
+    }
+}
+
---
+++ USB-Graphing-Meter/USB-Graphing-Meter/UI/FormMain.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using LibUsbDotNet;
+using LibUsbDotNet.Main;
+using Timer = System.Windows.Forms.Timer;
+
+namespace GraphingMeter
+{
+    public partial class FormMain : Form
+    {
+        private const int Vid = 0x16c0;
+        private const int Pid = 0x05df;
+
+        private readonly Timer _formUpdateTimer = new Timer();
+        private Thread _backThread;
+        private static UsbDevice _myUsbDevice;
+        private readonly SampleCounter _sampleCounter = new SampleCounter();
+        
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public FormMain()
+        {
+            InitializeComponent();
+
+            // Setup form update timer
+            _formUpdateTimer.Interval = 500;
+            _formUpdateTimer.Tick += FormUpdateTimer_Tick;
+            _formUpdateTimer.Start();
+        }
+
+        /// <summary>
+        /// From load event.
+        /// </summary>
+        private void FormTerminal_Load(object sender, EventArgs e)
+        {
+            // Attempt to find the usb device
+            FindUsbDevice();
+            
+            //fill in the x axis
+            for (int i = 0; i < 1000; i++)
+            {
+                chart1.Series[0].Points.Add(0);
+            }
+        }
+
+        /// <summary>
+        /// Form close event.
+        /// </summary>
+        private void FormTerminal_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            CloseUsbDevice();
+        }
+        
+        /// <summary>
+        /// formUpdateTimer Tick event to update terminal textbox.
+        /// </summary>
+        private void FormUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            // Update sample counter values
+            toolStripStatusLabelSampleRate.Text = "Sample Rate: " + _sampleCounter.SampleRate;
+        }
+
+        /// <summary>
+        /// Locates the usb device 
+        /// </summary>
+        private void FindUsbDevice()
+        {
+            // Find and open the usb device.
+            var usbFinder = new UsbDeviceFinder(Vid, Pid);
+            _myUsbDevice = UsbDevice.OpenUsbDevice(usbFinder);
+            if (_myUsbDevice == null)
+            {
+                return;
+            }
+
+            if (_myUsbDevice is IUsbDevice wholeUsbDevice)
+            {
+                // This is a "whole" USB device. Before it can be used, 
+                // the desired configuration and interface must be selected.
+                // Select config #1
+                wholeUsbDevice.SetConfiguration(1);
+
+                // Claim interface #0.
+                wholeUsbDevice.ClaimInterface(0);
+            }
+
+            try
+            {
+                _backThread = new Thread(Start)
+                {
+                    IsBackground = true
+                };
+                _backThread.Start();
+
+                _sampleCounter.Reset();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+        }
+
+        private void Start()
+        {
+            byte requestType = (0x01 << 5) | 0x80;
+            const byte request = 0x01;
+            short val = 0x03 << 8;
+            byte[] readBuffer = new byte[512];
+            var packet = new UsbSetupPacket(requestType, request, val, 0, 1);
+
+            while (true)
+            {
+                if (_myUsbDevice == null)
+                {
+                    return;
+                }
+
+                _myUsbDevice.ControlTransfer(ref packet, readBuffer, 512, out var transferred);
+
+                if (transferred != 0)
+                {
+                    this.Invoke((MethodInvoker)delegate 
+                    {
+                        OnReceiveEndPointData(readBuffer, transferred);
+                    });
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Event is fired each time samples are received from USB device
+        /// </summary>
+        private void OnReceiveEndPointData(byte[] buff, int count)
+        {
+            var series = chart1.Series[0];
+            // Process each byte
+            for (var index = 0; index < count; index++)
+            {
+                byte b = buff[index];
+                float number;
+                float.TryParse(tb_vcc.Text.ToString(), out number);
+                float volts = b / ( 255/ number);
+                lb_voltage.Text = volts.ToString("0.00") + " V";
+                series.Points.AddY(volts);
+                //series.Points.RemoveAt(0);
+
+                _sampleCounter.Increment();
+            }
+        }
+
+        /// <summary>
+        /// Closes serial port.
+        /// </summary>
+        private void CloseUsbDevice()
+        {
+            try
+            {
+                if (_backThread != null)
+                {
+                    _backThread.Abort();
+                }
+                if (_myUsbDevice != null)
+                {
+                    if (_myUsbDevice.IsOpen)
+                    {
+                        // If this is a "whole" usb device (libusb-win32, linux libusb-1.0)
+                        // it exposes an IUsbDevice interface. If not (WinUSB) the 
+                        // 'wholeUsbDevice' variable will be null indicating this is 
+                        // an interface of a device; it does not require or support 
+                        // configuration and interface selection.
+                        var wholeUsbDevice = _myUsbDevice as IUsbDevice;
+                        // Release interface #0.
+                        wholeUsbDevice?.ReleaseInterface(0);
+                        _myUsbDevice.Close();
+                    }
+                }
+
+                _myUsbDevice = null;
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+        
+    }
+}
